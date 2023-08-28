67fb6a7f8be134daa6031be4eac5aded56207d16
AshrefGw <AshGw@users.noreply.github.com>
2023-08-28 17:08:19 +0100

    Set data & crypto consts testing

---
+++ tests/test_consts.py
@@ -0,0 +1,20 @@
+import unittest
+
+from litecrypt.utils.consts import Size
+
+
+class ConstsTesting(unittest.TestCase):
+    def test_sizes(self):
+        assert Size.PEPPER % 16 == 0
+        assert Size.SALT % 16 == 0
+        assert Size.IV == 16
+        assert Size.BLOCK == 16
+        assert Size.MAIN_KEY == 32
+        assert Size.AES_KEY == 32
+        assert Size.HMAC == 32
+        assert Size.MIN_ITERATIONS == 50
+        assert Size.MAX_ITERATIONS == 10**6
+
+
+if __name__ == "__main__":
+    unittest.main()
