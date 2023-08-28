2e1b9127aa51db313e085773c0a5e4e1de062f5c
AshrefGw <AshGw@users.noreply.github.com>
2023-08-28 18:11:52 +0100

    Set cipher block sizes testing

--- tests/test_consts.py
+++ tests/test_consts.py
@@ -5,8 +5,8 @@ from litecrypt.utils.consts import Size
 
 class ConstsTesting(unittest.TestCase):
     def test_sizes(self):
-        assert Size.PEPPER % 16 == 0
-        assert Size.SALT % 16 == 0
+        assert Size.PEPPER >= 0 and Size.PEPPER % 16 == 0
+        assert Size.SALT >= 0 and Size.SALT % 16 == 0
         assert Size.IV == 16
         assert Size.BLOCK == 16
         assert Size.MAIN_KEY == 32
