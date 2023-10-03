c0b63a008aaa3d59325784b9454779dc6ba4dffb
AshrefGw <AshGw@users.noreply.github.com>
2023-10-03 23:28:31 +0100

    Refactored code

--- litecrypt/__init__.py
+++ litecrypt/__init__.py
@@ -1,5 +1,7 @@
+from litecrypt.core.base import DecBase, EncBase
 from litecrypt.core.datacrypt import Crypt, gen_key, gen_ref
 from litecrypt.core.filecrypt import CryptFile
 from litecrypt.mapper.database import Database, reference_linker, spawn
+from litecrypt.mapper.engines import get_engine
 
 __version__ = "0.0.7"
