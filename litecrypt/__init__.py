3d27d03b9ee04810a813b9100e8c679ea996ce51
AshrefGw <AshGw@users.noreply.github.com>
2023-10-03 22:51:50 +0100

    Bump version: 0.0.6 → 0.0.7

--- litecrypt/__init__.py
+++ litecrypt/__init__.py
@@ -2,4 +2,4 @@ from litecrypt.core.datacrypt import Crypt, gen_key, gen_ref
 from litecrypt.core.filecrypt import CryptFile
 from litecrypt.mapper.database import Database, reference_linker, spawn
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
