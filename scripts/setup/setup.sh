c6ee352faaec9ccaf9e004c726a00cce56e8eaa3
ashgw <ashrefgw@proton.me>
2023-11-27 13:48:03 +0100

    Setup fixed

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-git clone https://github.com/AshGw/litecrypt.git
-cd litecrypt || exit
-rm -rf dist
-rm -rf build
-rm -rf litecrypt.egg-info
 if [[ "$(uname -s)" == "MINGW"* ]]; then
   python.exe -m pip install --upgrade pip
 else
   pip install --upgrade pip
 fi
-pip install -r scripts/setup/requirements.txt
-python setup.py develop
-rm -rf litecrypt.egg-info
+echo "installing the project dependencies"
+pip install poetry
+poetry install
+rm -rf app.egg-info buildt
+
 pre-commit install
+cat <<EOF >> .git/hooks/pre-push
+#!/bin/bash
+
+echo "Before pushing, did you tie your shoes right ?"
+echo "Running tests.."
+pytest tests --no-header --no-summary -q
+
+if [ \$? -ne 0 ]; then
+  echo "Testing failed. Push aborted."
+  exit 1
+else
+  printf "Testing went successful!\nPushing changes.."
+fi
+
+exit 0
+EOF
+printf  "pre-push installed\nDevelopment environment ready!"
