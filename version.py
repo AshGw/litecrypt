e4e126333bb0f5fdcd697f7079581b16a5e28ff4
ashgw <ashrefgw@proton.me>
2023-11-27 14:01:30 +0100

    versioning function set

--- version.py
+++ version.py
@@ -3,5 +3,5 @@ import re
 with open("pyproject.toml") as f:
     content = f.read()
 
-version_match = re.search(r'version="(\d+\.\d+\.\d+)"', content)
+version_match = re.search(r'version = "(\d+\.\d+\.\d+)"', content)
 print(version_match.group(1))
