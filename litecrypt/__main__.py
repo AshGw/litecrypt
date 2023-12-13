0c515734b8da88fbeba2cc4a8a8d66e748e73fde
ashgw <ashrefgw@proton.me>
2023-12-13 23:57:27 +0100

    update warning for Py v3.7 and older

--- litecrypt/__main__.py
+++ litecrypt/__main__.py
@@ -3,12 +3,12 @@ import warnings
 
 from litecrypt.core.safepack.gui import main_object
 
-if sys.version_info < (3, 7):  # pragma: no cover
+if sys.version_info < (3, 8):  # pragma: no cover
     warnings.warn(
         message="""\033[38;5;214m
             This Python version is no longer supported by the Python team,
             nor is it supported by this project.
-            Python 3.7 or newer is required for this library to work properly.
+            Python 3.8 or newer is required for this library to work properly.
             Existing..\033[0m
         """,
         stacklevel=2,
